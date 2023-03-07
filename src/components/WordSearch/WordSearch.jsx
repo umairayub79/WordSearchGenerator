cbe846af1028fa219747483b27e5b505d0198db4
Umair Ayub <46655787+umairayub79@users.noreply.github.com>
2023-03-07 16:00:05 +0500

    Added Button to download as JSON

--- src/components/WordSearch/WordSearch.jsx
+++ src/components/WordSearch/WordSearch.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-import {Button} from "@material-tailwind/react"
+import { Button } from "@material-tailwind/react"
 import { MdDownload } from "react-icons/md"
 import charsets from "../../constants/Charsets.js"
-const WordSearch = ({words, size, charset, highlight}) => {
-  
+const WordSearch = ({ words, size, charset, highlight }) => {
+
   const [highlighted, setHighlighted] = useState([]); // state to store found words
   const [grid, setGrid] = useState([]); // state to store the word search grid
-  let textFileURL = '';
-  
+
   // function to generate a word search grid
-  function generateGrid(size, words, letters){
+  function generateGrid(size, words, letters) {
     const grid = []
     for (let i = 0; i < size; i++) {
       const row = [];
@@ -41,77 +40,110 @@ const WordSearch = ({words, size, charset, highlight}) => {
         // place the word in the grid
         for (let i = 0; i < word.length; i++) {
           grid[y + i * dy][x + i * dx] = word[i].toUpperCase();
-          items.push({ "row": y + i * dy, "col": x + i * dx, "letter": word[i].toUpperCase()})
-          
+          items.push({ "row": y + i * dy, "col": x + i * dx, "letter": word[i].toUpperCase() })
+
         }
       });
       setHighlighted(JSON.stringify(items))
-      console.log(JSON.stringify(items))
     }
 
     // update the state with the new grid
     setGrid(grid);
   }
 
-  // function to create a download link for the word search
-  function makeTextFile(text) {
-    let data = new Blob([text], {type: 'text/plain;charset=utf8'});
-    if (textFileURL !== null) {
-      URL.revokeObjectURL(textFileURL);
+  function downloadData(format) {
+    let data;
+    let contentType;
+    let fileExtension;
+    if (format === "text") {
+      const wordsearchString = grid.map(row => row.join(" ")).join("\n");
+      const textFileString = `${wordsearchString}\n\n${words.join("\n")}`;
+      data = textFileString;
+      contentType = "text/plain;charset=utf-8";
+      fileExtension = ".txt";
+    } else if (format === "json") {
+      data = { grid, words };
+      data = JSON.stringify(data, null, 2);
+      contentType = "application/json";
+      fileExtension = ".json";
+    } else {
+      console.error(`Invalid format specified: ${format}`);
+      return;
     }
-    textFileURL = URL.createObjectURL(data);
-    return textFileURL;
+
+    const now = new Date();
+    const timestamp = `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, "0")}${now
+      .getDate()
+      .toString()
+      .padStart(2, "0")}-${now
+        .getHours()
+        .toString()
+        .padStart(2, "0")}${now.getMinutes().toString().padStart(2, "0")}${now
+          .getSeconds()
+          .toString()
+          .padStart(2, "0")}`;
+    const filename = `WordSearch-${timestamp}${fileExtension}`;
+
+    const blob = new Blob([data], { type: contentType });
+    const link = document.createElement("a");
+
+    link.download = filename;
+    link.href = window.URL.createObjectURL(blob);
+    link.dataset.downloadurl = [contentType, link.download, link.href].join(":");
+
+    const evt = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+
+    try {
+      link.dispatchEvent(evt);
+    } catch (err) {
+      console.error(`Error while downloading ${format} file:`, err);
+    }
   }
 
-  // function to download the word search as a text file
-  function download() {
-    console.log('download')
-    let wordsearchLetters = grid.map(row => row.map(cell => cell));
-    let wordsearchString = wordsearchLetters.map(row => row.join(" ")).join("\n");
-
-    let wordsString = words.map(w => w).join("\n");
-    let textFileString = wordsearchString + "\n\n" + wordsString;
-    let fileName = `wordsearch-${Math.floor(Math.random() * 999)}.txt`
-    textFileURL = null
-    let url = makeTextFile(textFileString);
-    let element = document.createElement("a");
-    element.href = url;
-    element.download = fileName;
-    element.click();
-  }
 
   // generate the word search grid when the component mounts or the props change
   useEffect(() => {
-    generateGrid(size, words, charset === 'balochi'? charsets.balochi : charsets.english);
+    generateGrid(size, words, charset === 'balochi' ? charsets.balochi : charsets.english);
   }, [size, words, charset]);
 
   // render the word search grid
   return (
-  // Main container for the grid
-  <div className="flex flex-col items-center justify-center">
+    // Main container for the grid
     <div className="flex flex-col items-center justify-center">
-      {grid.map((row, rowIndex) => (
-        // Container for each row in the grid
-        <div key={rowIndex} className="w-screen flex flex-row items-center justify-center">
-          {row.map((item, colIndex) => (
-            // Container for each cell in the grid
-            <div key={colIndex} className={`h-8 w-8 text-center text-black ${highlight ? highlighted.includes(JSON.stringify({"row": rowIndex, "col": colIndex, "letter": item}))  ? 'bg-blue-600' : 'bg-white' : 'bg-white'}`}>
-              {item.toUpperCase()}
-            </div>
-          ))}
-        </div>
-      ))}
+      <div className="flex flex-col items-center justify-center">
+        {grid.map((row, rowIndex) => (
+          // Container for each row in the grid
+          <div key={rowIndex} className="w-screen flex flex-row items-center justify-center">
+            {row.map((item, colIndex) => (
+              // Container for each cell in the grid
+              <div key={colIndex} className={`h-8 w-8 text-center text-black ${highlight ? highlighted.includes(JSON.stringify({ "row": rowIndex, "col": colIndex, "letter": item })) ? 'bg-blue-600' : 'bg-white' : 'bg-white'}`}>
+                {item.toUpperCase()}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col">
+        {words.map((word, index) => (
+          <div key={index}>{word}</div>
+        ))}
+      </div>
+      <div className="flex gap-3">
+        <Button className="flex items-center mt-3" onClick={() => downloadData("text")}>
+          <MdDownload className="h-5 w-5" />
+          Download TEXT
+        </Button>
+        <Button className="flex items-center mt-3" onClick={() => downloadData("json")}>
+          <MdDownload className="h-5 w-5" />
+          Download JSON
+        </Button>
+      </div>
     </div>
-    <div className="flex flex-col">
-    {words.map((word, index) => (
-    <div key={index}>{word}</div>
-    ))}
-    </div>
-      <Button className="flex items-center mt-3" onClick={download}>
-        <MdDownload  className="h-5 w-5" />
-        Download txt File
-      </Button>
-  </div>
-)};
+  )
+};
 
 export default WordSearch;
